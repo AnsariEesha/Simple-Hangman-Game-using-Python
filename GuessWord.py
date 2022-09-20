3a16fcdb607caf56b8dbaa680e417cf1141647c9
Eesha Ansari <96630551+AnsariEesha@users.noreply.github.com>
2022-09-20 16:00:58 +0500

    Add files via upload

---
+++ GuessWord.py
@@ -0,0 +1,8 @@
+def GuessedWord(secret_word,letter_storage):
+    guess_word=''
+    for char in secret_word:
+        if char in letter_storage:
+            guess_word+=char
+        else:
+            guess_word+='_ '
+    return guess_word
